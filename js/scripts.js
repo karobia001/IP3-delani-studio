9fafd64b213216fac42600ba6574114ab251b7b0
karobia001 <karobiamaina81@gmail.com>
2019-08-25 17:19:37 +0300

    adds the main scripts function to the jquerry

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+// $(document).ready(function() {
+//     $("#design-image").click(function(){
+//         $("#design-image").toggle();
+//          $("#design-info").toggle();
+//     });
+//     $("#design-info").click(function(){
+//         $("#design-image").toggle();
+//          $("#design-info").toggle();
+//     });
+    // $("#dev-image").click(function(){
+    //     $("#dev-image").toggle();
+    //      $("#dev-info").toggle();
+    // });
+    // $("#dev-info").click(function(){
+    //     $("#dev-image").toggle();
+    //      $("#dev-info").toggle();
+    // });
+    // $("#prod-image").click(function(){
+    //     $("#prod-image").toggle();
+    //      $("#prod-info").toggle();
+    // });
+    // $("#prod-info").click(function(){
+    //     $("#prod-image").toggle();
+    //      $("#prod-info").toggle();
+    // });
+    
+    
+//});
