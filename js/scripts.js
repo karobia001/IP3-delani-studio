85f8d7433822c04940c729b63640bd26baeaf935
karobia001 <karobiamaina81@gmail.com>
2019-08-26 11:41:23 +0300

    adds the README file

--- js/scripts.js
+++ js/scripts.js
@@ -19,9 +19,53 @@
           $("#prod-info").toggle();
      });
      $("#prod-info").click(function(){
-         $("#prod-image").toggle();
-          $("#prod-info").toggle();
+        $("#prod-image").toggle();
+         $("#prod-info").toggle();
+    });
+
+     $("#image1").hover(function(){
+         $(".info1").toggle(200);
      });
-    
-    
+     $("#image2").hover(function(){
+        $(".info2").toggle(200);
+    });
+    $("#image3").hover(function(){
+        $(".info3").toggle(200);
+    });
+    $("#image4").hover(function(){
+        $(".info4").toggle(200);
+    });
+    $("#image5").hover(function(){
+        $(".info5").toggle(200);
+    });
+    $("#image6").hover(function(){
+        $(".info6").toggle(200);
+    });
+    $("#image7").hover(function(){
+        $(".info7").toggle(200);
+    });
+    $("#image8").hover(function(){
+        $(".info8").toggle(200);
+    });
+   
 });
+
+
+
+function valid(){
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var text = document.getElementById("textarea").value;
+
+    if(name === ""){
+        alert("Insert name!");
+
+    }else if(email === ""){
+        alert("Insert email!");
+    }else if(text === ""){
+        alert("no message to be submitted");
+    }else{
+        alert("We have recieved you message dear " + name);
+    };
+
+};
